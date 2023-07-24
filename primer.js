fcec36d33beb12c6d9b347f55fe5baaa1cd0bd9b
Sergey Elshin <elshin@proitr.ru>
2023-07-24 16:22:20 +0500

    параметр по умолчанию

--- primer.js
+++ primer.js
@@ -1,1 +1,6 @@
-console.log("Hello1");
+var myFunc = function (name, weather) {
+    if (weather === void 0) { weather = "raining"; }
+    console.log("Hello " + name + ".");
+    console.log("It is " + weather + " today");
+};
+myFunc("Adam");
--- primer.ts
+++ primer.ts
@@ -1,6 +1,6 @@
-let myFunc = function(name, weather) {
+let myFunc = function (name, weather = "raining") {
     console.log("Hello " + name + ".");
     console.log("It is " + weather + " today");
 };
 
-myFunc("Adam", "sunny");
+myFunc("Adam");
